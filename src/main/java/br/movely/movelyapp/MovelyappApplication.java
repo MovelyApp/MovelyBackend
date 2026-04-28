49da39536f467cfd54aa5fcc0232762662c4f504
Carolina Amorim <carolinaoa1@al.insper.edu.com>
2026-04-28 16:15:53 -0300

    Initial Spring Setup

---
+++ src/main/java/br/movely/movelyapp/MovelyappApplication.java
@@ -0,0 +1,13 @@
+package br.movely.movelyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovelyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovelyappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/movely/movelyapp/MovelyappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.movely.movelyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovelyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
