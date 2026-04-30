92993649d443a9f8f7d5642db27b933ac4a3899a
Carolina Amorim <carolinaoa1@al.insper.edu.com>
2026-04-30 11:52:42 -0300

    ajustes e endpoints das subclasses

---
+++ src/main/java/br/movely/movelyapp/DTO/CreateStepsGoalDTO.java
@@ -0,0 +1,15 @@
+package br.movely.movelyapp.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class CreateStepsGoalDTO {
+    private UUID challengeId;
+    private String name;
+    private String description;
+    private int stepsTarget;
+}
