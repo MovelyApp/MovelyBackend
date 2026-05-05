6c2b1ee9948862b7be6dea3570e6c9d26ed5e1f7
Rafaela Coutinho <rafaelamc4@al.insper.edu.br>
2026-05-05 10:11:36 -0300

    feat: site link

--- src/main/java/br/movely/movelyapp/config/SecurityConfig.java
+++ src/main/java/br/movely/movelyapp/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://movely-frontend.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
